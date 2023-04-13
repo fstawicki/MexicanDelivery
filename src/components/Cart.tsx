89cfb12bb75a8117d9cadd5b1f4f791e00a59bfa
fstawicki <stawickifilip1@gmail.com>
2023-04-13 22:16:51 +0200

    working on Cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,12 +1,18 @@
 import {FC} from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
 import { FaShoppingCart } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
+import { BiArrowBack } from 'react-icons/bi';
 
 import '../styles/Cart.scss';
 
 interface Props{}
 
 const Cart: FC<Props> = () => {
+
+    const navigate = useNavigate();
 
     return(
 
@@ -24,7 +30,8 @@ const Cart: FC<Props> = () => {
                 <input type="number" name="" id="" />
                 <p className='deleteItemBtn'><a href=""><MdDeleteForever/></a></p>
             </div>
-            <button className="toCheckout">Go to Checkout</button>
+            <button className="btn backToMenuBtn" onClick={() => {navigate('/');}}><BiArrowBack/><span>Back to Menu</span></button>
+            <button className="btn toCheckout"><FaShoppingCart /> Go to Checkout</button>
         </div>
     );
 
